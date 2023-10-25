ea416d5a378bf090a61e38a2daa98286e8502377
Abir <abirmahmud221@gmail.com>
2023-10-25 11:14:05 +0600

    success

--- src/components/SuccessStory/SuccessStory.jsx
+++ src/components/SuccessStory/SuccessStory.jsx
@@ -15,12 +15,8 @@ const SuccessStory = () => {
                                 R&D Lab
                             </div>
                             <div className="collapse-content">
-                                <p>Details: <br />
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit dolores repellat possimus
-                                    unde neque nostrum, rem, exercitationem debitis reiciendis in fugiat, sint eveniet?
-                                    Sapiente tempora eum nemo, iure vel nesciunt eligendi enim ex nostrum doloribus at
-                                    maxime adipisci optio soluta officia quia aspernatur. Hic, dicta. Animi est eum voluptas
-                                    vitae?</p>
+                                <p>
+                                We've been engaged in a Memorandum of Understanding (MOU) with BEACON Pharmaceutical, collaborating closely on the design of an oncology lab. This lab is intended for the production of finished fine chemicals used in the manufacturing of cancer drugs, specifically at the gram to kilo scale. Our dedicated team has successfully completed 80% of the design work, showcasing our commitment to delivering high-quality solutions for this critical endeavor. However, due to management decisions, the project is currently on hold. We remain ready and eager to resume and complete this important project whenever it aligns with BEACON Pharmaceutical's objectives and priorities.</p>
                             </div>
                         </div>
                         <div className="collapse collapse-arrow bg-base-200">
@@ -29,20 +25,16 @@ const SuccessStory = () => {
                                 Car Engine
                             </div>
                             <div className="collapse-content">
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium tempora quos velit
-                                    cum nemo ad incidunt voluptatibus, eligendi vero, labore facere reiciendis minima.
-                                    Expedita error reprehenderit laborum veniam in reiciendis!</p>
+                                <p>We have successfully supplied the Toyota 60 car engine to a distinguished company in Bangladesh. Their innovative approach involves repurposing and reassembling older vehicles to enhance cost-efficiency and reliability. This engine, known for its exceptional performance, will play a crucial role in revitalizing and ensuring the continued reliability of their fleet, aligning perfectly with their commitment to sustainable practices and dependable service.</p>
                             </div>
                         </div>
                         <div className="collapse collapse-arrow bg-base-200">
                             <input type="radio" name="my-accordion-2" />
                             <div className="collapse-title text-xl font-medium">
-                                Heavy vehicles battery
+                            Heavy Forklift  Battery
                             </div>
                             <div className="collapse-content">
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium tempora quos velit
-                                    cum nemo ad incidunt voluptatibus, eligendi vero, labore facere reiciendis minima.
-                                    Expedita error reprehenderit laborum veniam in reiciendis!</p>
+                                <p>We take pride in delivering 23 forklift batteries, carefully selected from esteemed brands like Heli, Zoomlion, and Toyota. These batteries, offered with a 3-year warranty, are specifically "Made For Japan," assuring the highest quality and cost efficiency. We are committed to providing our customers with top-tier products that reflect our dedication to excellence and reliability.</p>
                             </div>
                         </div>
                         <div className="collapse collapse-arrow bg-base-200">
@@ -51,20 +43,16 @@ const SuccessStory = () => {
                                 Car Import
                             </div>
                             <div className="collapse-content">
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium tempora quos velit
-                                    cum nemo ad incidunt voluptatibus, eligendi vero, labore facere reiciendis minima.
-                                    Expedita error reprehenderit laborum veniam in reiciendis!</p>
+                                <p>We are proud to offer our valued customers a unique and hassle-free car buying experience. With our exclusive membership to Japan Auctions, we can source and procure your desired vehicle, whether it's a Japanese brand like Harrier, Hiace, Axio, Primio, or X-Trail, or renowned German brands like BMW and Mercedes-Benz. Our services extend to providing convenient door-to-door delivery, ensuring a seamless process without the need for you to deal with freight or customs-related activities. Your satisfaction and convenience are our top priorities, making your car purchase a smooth and enjoyable experience</p>
                             </div>
                         </div>
                         <div className="collapse collapse-arrow bg-base-200">
                             <input type="radio" name="my-accordion-2" />
                             <div className="collapse-title text-xl font-medium">
-                                Sound System
+                            Manufactuirng Tools
                             </div>
                             <div className="collapse-content">
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium tempora quos velit
-                                    cum nemo ad incidunt voluptatibus, eligendi vero, labore facere reiciendis minima.
-                                    Expedita error reprehenderit laborum veniam in reiciendis!</p>
+                                <p>We take pride in supplying a range of essential thread cutting tools sourced from Japan, which are indispensable in various industries as a cornerstone of manufacturing processes. These tools are renowned for their exceptional durability, ensuring a long-term cost-efficient solution for our customers. We understand the critical role these tools play in production, and our commitment to quality and reliability makes us your trusted partner in achieving manufacturing excellence</p>
                             </div>
                         </div>
                     </div>
